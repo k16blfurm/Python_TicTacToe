2896bf9516bb4d3e111e0f9a8c17ed9b3f953f41
k16blfurm <bfurman@hawaii.edu>
2019-06-19 03:56:04 +0000

    fixed output to terminal. Trying to add filtering of bad inputs.

--- tictactoe.py
+++ tictactoe.py
@@ -19,30 +19,79 @@ def funcl(brd):
 def newGAME():
 	boardNEW = [[0,0,0],[0,0,0],[0,0,0]]
 
-def a1():
-	return 
+def row1(column, brd):
+	if column == 'a':
+		brd[0][0] = 4
+		return brd
+	if column == 'b':
+		brd[0][1] = 4
+		return brd
+	if column == 'c':
+		brd[0][2] = 4
+		return brd
 
-def boardMOVEMENT(brd):
-	hello= 9	
+def row2(column, brd):                                                     
+        if column == 'a':                                                  
+                brd[1][0] = 4                                              
+                return brd                                                 
+        if column == 'b':                                                  
+                brd[1][1] = 4                                              
+                return brd                                                 
+        if column == 'c':                                                  
+                brd[1][2] = 4                                              
+                return brd
 
+def row3(column, brd):                                                     
+        if column == 'a':                                                  
+                brd[2][0] = 4                                              
+                return brd                                                 
+        if column == 'b':                                                  
+                brd[2][1] = 4                                              
+                return brd                                                 
+        if column == 'c':                                                  
+                brd[2][2] = 4                                              
+                return brd
 
+def boardMOVEMENT(brd, ROW, COLUMN):
+	if ROW == '1':
+		print('here')
+		x = row1(COLUMN, brd)
+		return x
+	if ROW == '2':
+		y = row2(COLUMN, brd)
+		return y
+	if ROW == '3':
+		z = row3(COLUMN, brd)
+		return z
+
+#this is the original board
 boardREAL = [[0,0,0],[0,0,0],[0,0,0]]
+#this is to show the user first time how to
+#play the game
 first = 1
-guide = 'enter, for example "a1" or "c3"'
+guide = 'enter, for example "3", then enter "a"'
 s = 'Welcome to my tic tac toe game\n'
 str(s)
 print(s)
 gameOVER = False
 funcl(boardREAL)
-
+#this is for filtering bad inputs
+inputROW = False
 
 while gameOVER != True:
 	print('enter a spot to place your piece/n')
 	if(first == 1):
 		print(guide)
 		first = 0
-	movement = input()
+	while(inputROW == False):
+		#this is for filtering bad inputs
+		movementROW = input()
+		if movementROW != '1' or '2' or '3':
+			inputROW = True
+		print('here')
+
 	print('now enter your column')
+	movementCOLUMN = input()
 	#for adding movement to board
-	boardREAL = boardMOVEMENT(boardREAL)
+	boardREAL = boardMOVEMENT(boardREAL,movementROW,movementCOLUMN)
 	funcl(boardREAL)
