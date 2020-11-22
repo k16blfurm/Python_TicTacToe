37f4eb21f2121005dbc2eab1012ccb4628b0742d
k16blfurm <bfurman@hawaii.edu>
2020-11-21 18:11:46 -0800

    This change has made the board able to be drawn

--- tictactoe.py
+++ tictactoe.py
@@ -3,15 +3,15 @@ from array import *
 
 def funcl(brd):
 	tic = ("   a     b     c\n"                                                
-	"      |     |     \n"                                                     
-	"1  %d  |  %d  |  %d  \n"                                                     
-	" _____|_____|_____\n"                                                     
-	"      |     |     \n"                                                     
-	"2  %d  |  %d  |  %d  \n"                                                     
-	" _____|_____|_____\n"                                                     
-	"      |     |     \n"                                                     
-	"3  %d  |  %d  |  %d  \n"                                                     
-	"      |     |     \n"  
+	       "      |     |     \n"                                                     
+	       "1  %d  |  %d  |  %d  \n"                                                     
+	       " _____|_____|_____\n"                                                     
+	       "      |     |     \n"                                                     
+	       "2  %d  |  %d  |  %d  \n"                                                     
+	       " _____|_____|_____\n"                                                     
+               "      |     |     \n"                                                     
+	       "3  %d  |  %d  |  %d  \n"                                                     
+	       "      |     |     \n"  
 %(brd[0][0],brd[0][1],brd[0][2],brd[1][0],brd[1][1],brd[1][2],brd[2][0],brd[2][1],brd[2][2]))
 	print(tic)
 
@@ -31,26 +31,26 @@ def row1(column, brd):
 		return brd
 
 def row2(column, brd):                                                     
-        if column == 'a':                                                  
-                brd[1][0] = 4                                              
-                return brd                                                 
-        if column == 'b':                                                  
-                brd[1][1] = 4                                              
-                return brd                                                 
-        if column == 'c':                                                  
-                brd[1][2] = 4                                              
-                return brd
+		if column == 'a':                                                  
+			brd[1][0] = 4                                              
+			return brd                                                 
+		if column == 'b':                                                  
+			brd[1][1] = 4                                              
+			return brd                                                 
+		if column == 'c':                                                  
+			brd[1][2] = 4                                              
+			return brd
 
 def row3(column, brd):                                                     
-        if column == 'a':                                                  
-                brd[2][0] = 4                                              
-                return brd                                                 
-        if column == 'b':                                                  
-                brd[2][1] = 4                                              
-                return brd                                                 
-        if column == 'c':                                                  
-                brd[2][2] = 4                                              
-                return brd
+		if column == 'a':                                                  
+			brd[2][0] = 4                                              
+			return brd                                                 
+		if column == 'b':                                                  
+			brd[2][1] = 4                                              
+			return brd                                                 
+		if column == 'c':                                                  
+			brd[2][2] = 4                                              
+			return brd
 
 def boardMOVEMENT(brd, ROW, COLUMN):
 	if ROW == '1':
@@ -77,6 +77,7 @@ gameOVER = False
 funcl(boardREAL)
 #this is for filtering bad inputs
 inputROW = False
+inputColumn = False
 
 while gameOVER != True:
 	print('enter a spot to place your piece/n')
@@ -86,12 +87,26 @@ while gameOVER != True:
 	while(inputROW == False):
 		#this is for filtering bad inputs
 		movementROW = input()
-		if movementROW != '1' or '2' or '3':
+		if movementROW == '1' or '2' or '3':
 			inputROW = True
-		print('here')
+		if(inputROW == False):
+			print('re-enter a correct value')
+			movementROW = ''
 
 	print('now enter your column')
-	movementCOLUMN = input()
+
+	while(inputColumn == False):
+		movementCOLUMN = input()
+		if movementCOLUMN == 'a' or 'b' or 'c':
+			inputColumn =  True
+		if(inputColumn == False):
+			print('re-enter a correct value')
+			movementCOLUMN = ''
+
 	#for adding movement to board
 	boardREAL = boardMOVEMENT(boardREAL,movementROW,movementCOLUMN)
 	funcl(boardREAL)
+
+	# this if for clearing the inputs after sent to board
+	movementCOLUMN = ''
+	movementROW = ''
